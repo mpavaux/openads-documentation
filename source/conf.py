ebc926329a1ee6ea403d11b56b07a5775a8889fd
Florent Michon <fmichon@atreal.fr>
2013-11-05 15:04:59 +0100

    Changement du thème pour readthedocs

--- source/conf.py
+++ source/conf.py
@@ -13,6 +13,9 @@
 
 import sys, os
 
+# ReadTheDocs Flag
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -92,7 +95,10 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = 'sphinxdoc'
-html_theme = 'sphinx.openmairietheme'
+if on_rtd:
+    html_theme = 'default'
+else:
+    html_theme = 'sphinx.openmairietheme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
